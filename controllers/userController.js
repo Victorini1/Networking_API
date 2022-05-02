29eda4e60dccff9cfa5cfe562567a4c44e86c5da
Baeonetta <victor.m.kennedy@gmail.com>
2022-05-02 14:59:00 -0500

    updated user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,22 +23,67 @@ module.exports = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
+
+  // update a user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // delete a user
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user with this id!' })
+          : res.json({ message: 'User successfully deleted!' })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // add new friend to user's friend list
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: { _id: req.params.friendId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // delete friend from friend list
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
-
-//update a user
-const UpdateSingleUser = async (req, res) => {
-try{
-  User.findOneAndUpdate( 
-    const updatedUser =
-    { _id: req.params.userId},
-    {$set: req.body},
-    {runValidators: true, new: true});
-    res.json(updatedUser)
-}
-}
-
- const deleteUser = async (req, res) => {
-   try{
-     const dele
-   }
- }   
